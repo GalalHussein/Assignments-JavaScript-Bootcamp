7225a0a098a23b02d171c4d7d2d72d1231529073
Galal <159665895+GalalHussein@users.noreply.github.com>
2025-01-25 14:34:46 +0200

    Add files via upload

---
+++ ass_4.js
@@ -0,0 +1,41 @@
+// Ass_1:
+// console.log(100000);
+// console.log(100_000);
+// console.log(1e5);
+// console.log(5e4 + 5e4);
+// console.log(Math.pow(10, 5));
+// console.log(Math.pow(10, 2) * Math.pow(10, 3));
+// console.log(+"100000");
+// console.log(10_0_0_0_0);
+// console.log(Math.pow(Math.pow(2,  3) + 2, 5));
+// console.log(10*10*10*10*10);
+
+// Ass_2:
+console.log(Number.MIN_SAFE_INTEGER *-1);
+
+// Ass_3:
+console.log(Number.MAX_SAFE_INTEGER.toString().length);
+
+// Ass_4:
+let myVar = "100.56789 Views";
+console.log(parseInt(myVar)); // 100
+console.log(+parseFloat(myVar).toFixed(2)); // 100.57
+
+// Ass_5:
+let num = 10;
+console.log(Number.isInteger(num) + num / num); // 2
+
+
+// Ass_6:
+let flt = 10.4;
+console.log(+flt.toFixed(0)); // 10
+console.log(Math.trunc(flt)); // 10
+console.log(Math.round(flt)); // 10
+console.log(Math.floor(flt)); // 10
+console.log(Math.ceil(flt) - flt / flt); // 10
+
+// Ass_7:
+console.log(Math.trunc((Math.random() * 5)));
+
+
+
