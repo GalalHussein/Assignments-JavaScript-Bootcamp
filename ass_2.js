6bc2915305f02f28f0d69f258ad10d8d13496087
Galal <159665895+GalalHussein@users.noreply.github.com>
2025-01-25 10:44:42 +0200

    Add files via upload

--- ass_2.js
+++ ass_2.js
@@ -1,23 +1,23 @@
 // ass_1:
-// let number_one = 10, number_two = 20;
-// console.log(number_one + '' + number_two);
-// console.log(typeof (number_one + '' + number_two));
-// console.log(`${number_one}${number_two}`);
-// console.log(typeof `${number_one}${number_two}`);
-// console.log(number_one + '\n' + number_two);
-// console.log(`${number_one}\n${number_two}`);
+let number_one = 10, number_two = 20;
+console.log(number_one + '' + number_two);
+console.log(typeof (number_one + '' + number_two));
+console.log(`${number_one}${number_two}`);
+console.log(typeof `${number_one}${number_two}`);
+console.log(number_one + '\n' + number_two);
+console.log(`${number_one}\n${number_two}`);
 
 // ass_2:
 // Write in HTML file: <h1 id="elzero">Object</h1>
-// console.log(elzero.innerHTML); // object
-// console.log(typeof elzero); // object
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
 
 // ass_3:
-// console.log("`I'm In\n \\\\ \n Love \\\\ \"\"\" \'\'\'\n++ with ++\n\\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"``");
+console.log("`I'm In\n \\\\ \n Love \\\\ \"\"\" \'\'\'\n++ with ++\n\\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"``");
 
 //ass_4:
-// let a = 20, b = 21;
-// console.log('_' + b + '_' + a + '' + b + '_' + a + '' + b + '_' + a + '' + b +'_'+ a + '_');
+let a = 20, b = 21;
+console.log('_' + b + '_' + a + '' + b + '_' + a + '' + b + '_' + a + '' + b +'_'+ a + '_');
 
 
 
