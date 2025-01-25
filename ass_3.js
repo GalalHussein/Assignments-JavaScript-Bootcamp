dae7388dd17f83059d7abf1f1a3b3e66252db5fe
Galal <159665895+GalalHussein@users.noreply.github.com>
2025-01-25 11:09:28 +0200

    Add files via upload

---
+++ ass_3.js
@@ -0,0 +1,27 @@
+// Ass_1:
+// console.log(10 >> 20 + 15 + 3 + 190 + 10 + 400); // 0
+
+// Ass_2:
+// let num = 3;
+// console.log(num + true + true + true);
+// console.log(num + num);
+// console.log(num << num / num);
+// console.log(num + num);
+// console.log(num * (num - true));
+// console.log(num << true + (num >> true) / (true << true));
+
+// Ass_3:
+// let x = "10";
+// console.log(+x << true);
+// console.log(+x + +x);
+// console.log(+x * true << true);
+// console.log(+x / (x >> true) * x);
+// console.log(+x * (true + true));
+
+// Ass_4:
+// let points = 10;
+// console.log(points + (true << true) + true); // 13
+// console.log(points - (true << true)); // 8;
+
+
+
